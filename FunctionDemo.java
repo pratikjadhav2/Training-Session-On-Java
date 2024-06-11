1627988ca7355cb797d31df7acc1d5c5ec0332fe
pratikjadhav2 <pratik98333@gmail.com>
2024-06-11 15:00:06 +0530

    Add files via upload

---
+++ FunctionDemo.java
@@ -0,0 +1,18 @@
+package sam.com;
+
+public class FunctionDemo {
+	public static void add() {
+		int a=10;
+		int b=20;
+		int total=a+b;
+		System.out.println("Addition="+total);
+		
+	}
+	public static void sub(int i,int j) {
+	
+	     int total=i-j;
+		System.out.println("Substraction="+total);
+		
+	}
+
+}
---
+++ TestFunction.java
@@ -0,0 +1,11 @@
+package sam.com;
+
+public class TestFunction {
+	
+	public static void main(String args[])
+	{
+		 FunctionDemo.add();
+		 FunctionDemo.sub(20,10);//calling function
+	}
+
+}
